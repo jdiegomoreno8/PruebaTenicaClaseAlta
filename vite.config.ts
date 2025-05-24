1ecd91e7f634e083ae584ee0240884e7e56f6a9c
juan diego ospina moreno <jdiegomoreno8@gmail.com>
2025-05-23 19:09:55 -0500

    Proyecto listo para despliegue

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://github.com/jdiegomoreno8/PruebaTenicaClaseAlta.git',  // reemplaza con el nombre exacto de tu repo en GitHub
   plugins: [vue()],
 })
